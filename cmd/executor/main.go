c0caf814d301097ac17ad622ec52d61b25e52ded
Sascha Andres <sascha.andres@outlook.com>
2018-08-13 12:39:07 +0200

    feat: run command for each directory with label

--- cmd/executor/main.go
+++ cmd/executor/main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"gopkg.in/yaml.v1"
+	"io/ioutil"
+	"livingit.de/code/run4labelled"
+	"os"
+	"os/exec"
+	"strings"
+)
 
 func main() {
-	fmt.Println("vim-go")
+
+	var configFileName = flag.String("config", "", "configuration file")
+	flag.Parse()
+
+	if *configFileName == "" {
+		panic("no configuration file found")
+	}
+
+	data, err := ioutil.ReadFile(*configFileName)
+	if err != nil {
+		panic(err)
+	}
+
+	var cfg run4labelled.Configuration
+	err = yaml.Unmarshal(data, &cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	rec := make(chan run4labelled.Execute)
+	cfg.SetChannel(rec)
+
+	go func() {
+		if err = cfg.Walk(dir); err != nil {
+			panic(err)
+		}
+	}()
+
+	for i := range rec {
+		params := strings.Split(i.Command, " ")
+		c := exec.Cmd{
+			Dir:    i.Directory,
+			Path:   params[0],
+			Args:   params[0:],
+			Stdout: os.Stdout,
+		}
+		err := c.Run()
+		if err != nil {
+			panic(err)
+		}
+	}
 }
--- types.go
+++ types.go
@@ -1,18 +1,66 @@
 package run4labelled
 
+import (
+	"github.com/pkg/errors"
+	"os"
+	p "path"
+	"path/filepath"
+	"strings"
+)
+
 type (
 	// Configuration contains all data required to run the program
 	Configuration struct {
-		Label    string   // Label is the name of the file to look for
-		Excludes []string // Excludes is a list of directory names to ignore
-		Run      string   // Run is executed in each labelled directory
+		Label    string   `yaml:"label"`    // Label is the name of the file to look for
+		Excludes []string `yaml:"excludes"` // Excludes is a list of directory names to ignore
+		Run      string   `yaml:"run"`      // Run is executed in each labelled directory
 
-		sendChannel <-chan Execute // sendChannel is used to tell executor to xecute other procexx
+		sendChannel chan Execute // sendChannel is used to tell executor to execute other process
 	}
 
 	// Execute is sent to executor with relevant information
 	Execute struct {
 		Directory string
-		Command   []string
+		Command   string
 	}
 )
+
+// SetChannel must be called to allow the logic to send commands to be executed to the executor
+func (c *Configuration) SetChannel(toExecutor chan Execute) error {
+	if nil == toExecutor {
+		return errors.New("no channel provided")
+	}
+	c.sendChannel = toExecutor
+	return nil
+}
+
+func (c *Configuration) Walk(baseDir string) error {
+	if nil != c.sendChannel {
+		defer close(c.sendChannel)
+	}
+	if s, err := os.Stat(baseDir); os.IsNotExist(err) || !s.IsDir() {
+		return errors.New("base directory not fine")
+	}
+	err := filepath.Walk(baseDir, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			for _, val := range c.Excludes {
+				if strings.HasSuffix(path, val) {
+					return filepath.SkipDir
+				}
+			}
+			if _, err := os.Stat(p.Join(path, c.Label)); err == nil {
+				if nil != c.sendChannel {
+					c.sendChannel <- Execute{
+						Directory: path,
+						Command:   c.Run,
+					}
+				}
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
